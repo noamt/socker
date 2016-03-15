270f4374b148b9030440ddfc76e84b7d20525e48
noamt <noam@10ne.org>
2016-03-16 00:47:13 +0200

    Adding first server impl

---
+++ server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "flag"
+    "go/build"
+    "log"
+    "net/http"
+    "path/filepath"
+    "text/template"
+)
+
+var chatWindowTemplate *template.Template
+
+func main() {
+    log.Println("Starting Socker server...")
+    address := flag.String("address", "localhost:8080", "The address of the Socker server")
+    flag.Parse()
+    chatWindowTemplate = template.Must(template.ParseFiles(filepath.Join(resources(), "index.html")))
+    http.HandleFunc("/", chatWindowHandler)
+    err := http.ListenAndServe(*address, nil)
+    if err != nil {
+        log.Fatal("Failed to start Socker server", err)
+    }
+}
+
+func resources() string {
+    p, err := build.Default.Import("github.com/noamt/socker/server/resources", "", build.FindOnly)
+    if err != nil {
+        return "."
+    }
+    return p.Dir
+}
+
+func chatWindowHandler(response http.ResponseWriter, request *http.Request) {
+    chatWindowTemplate.Execute(response, request.Host)
+}
